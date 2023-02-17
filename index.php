15b202bc24f8a09b93ac9939fcc86e134cdccc88
Nick Andronov <nickandronick@gmail.com>
2023-02-17 15:18:39 +0300

    initial commit: registration and login

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+$website_title = 'Главная';
+require 'blocks/head.php';
+?>
+</head>
+<body>
+<?php require 'blocks/header.php'; ?>
+<main class="container mt-5">
+  <div class="row">
+      <div class="col-md-8 mb-5">
+          Основная часть страницы
+      </div>
+      <?php require 'blocks/aside.php'; ?>
+  </div>
+</main>
+    
+<?php require 'blocks/footer.php'; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
